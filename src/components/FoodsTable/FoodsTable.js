355c741a8f8923144bcfd9a7544f581df04d5aaf
Guy Tepper <guytepper@gmail.com>
2017-09-16 15:57:38 +0300

    Added option to filter food names

--- src/components/FoodsTable/FoodsTable.js
+++ src/components/FoodsTable/FoodsTable.js
@@ -1,39 +1,96 @@
-import React from 'react';
-import { Table } from 'antd';
-import foods from '../../data/foods.json';
+import React, { Component } from 'react';
+import { Table, Input, Button, Icon } from 'antd';
+import data from '../../data/foods.json';
 import './FoodsTable.css';
+  
+class FoodsTable extends Component {
+  state = {
+    filterDropdownVisible: false,
+    data,
+    searchText: '',
+    filtered: false,
+  };
 
-const columns = [{
-  title: 'Name',
-  dataIndex: 'title',
-  width: 320,
-}, {
-  title: 'Amount',
-  dataIndex: 'amount',
-}, {
-  title: 'Soluble',
-  dataIndex: 'soluble',
-  sorter: (a, b) => a.soluble - b.soluble
-}, {
-  title: 'Insoluble',
-  dataIndex: 'insoluble',
-  sorter: (a, b) => a.insoluble - b.insoluble
-}, {
-  title: 'Total',
-  dataIndex: 'total',
-  sorter: (a, b) => a.total - b.total
-}];
+  onInputChange = (e) => {
+    this.setState({ searchText: e.target.value });
+  }
 
-function FoodsTable () {
-  return (
-    <Table
-      className="foods-table"
-      columns={columns}
-      dataSource={foods}
-      bordered={true}
-      size={'medium'}
-    />
-  )
+  onSearch = () => {
+    const { searchText } = this.state;
+    const reg = new RegExp(searchText, 'gi');
+    this.setState({
+      filterDropdownVisible: false,
+      filtered: !!searchText,
+      data: data.map(record => {
+        const match = record.title.match(reg);      
+        if (!match) {
+          return null;
+        }
+        return {
+          ...record,
+          title: (
+            <span>
+              {record.title.split(reg).map((text, i) => (
+                i > 0 ? [<span className="highlight">{match[0]}</span>, text] : text
+              ))}
+            </span>
+          ),
+        };
+      }).filter(record => !!record),
+    });
+  }
+    
+  render() {
+    const columns = [{
+      title: 'Name',
+      dataIndex: 'title',
+      width: 320,
+      filterDropdown: (
+        <div className="custom-filter-dropdown">
+          <Input
+            ref={ele => this.searchInput = ele}
+            placeholder="Search name"
+            value={this.state.searchText}
+            onChange={this.onInputChange}
+            onPressEnter={this.onSearch}
+          />
+          <Button type="primary" onClick={this.onSearch}>Search</Button>
+        </div>
+      ),
+      filterIcon: <Icon type="search" style={{ color: this.state.filtered ? '#108ee9' : '#aaa' }} />,
+      filterDropdownVisible: this.state.filterDropdownVisible,
+      onFilterDropdownVisibleChange: (visible) => {
+        this.setState({
+          filterDropdownVisible: visible,
+        }, () => this.searchInput.focus());
+      },
+    }, {
+      title: 'Amount',
+      dataIndex: 'amount',
+    }, {
+      title: 'Soluble',
+      dataIndex: 'soluble',
+      sorter: (a, b) => a.soluble - b.soluble
+    }, {
+      title: 'Insoluble',
+      dataIndex: 'insoluble',
+      sorter: (a, b) => a.insoluble - b.insoluble
+    }, {
+      title: 'Total',
+      dataIndex: 'total',
+      sorter: (a, b) => a.total - b.total
+    }];
+
+    return (
+      <Table
+        className="foods-table"
+        columns={columns}
+        dataSource={this.state.data}
+        bordered={true}
+        size={'medium'}
+      />
+    )
+  }
 }
 
 export default FoodsTable;
